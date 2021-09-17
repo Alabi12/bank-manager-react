6da8f4658d02aa82b1da6244fd652497a1c4ff9a
Alabi12 <71880257+Alabi12@users.noreply.github.com>
2021-09-17 00:18:25 +0000

    Add files via upload

---
+++ precache-manifest.c42cafc41c3112720ab1ddf18e80c42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806bcd1969fd300177cbfa1e94fde24a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da8dcc314e6b6b5d4b84",
+    "url": "/static/css/main.c05c03dd.chunk.css"
+  },
+  {
+    "revision": "48ffce80de9b93786708",
+    "url": "/static/js/2.b6120392.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b6120392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8dcc314e6b6b5d4b84",
+    "url": "/static/js/main.ecf418e1.chunk.js"
+  },
+  {
+    "revision": "2bf6298f567f9896b541",
+    "url": "/static/js/runtime-main.23e5a4b0.js"
+  }
+]);
